00ae37c8b14c77a4efbcfeb624186d5771623e02
Arkashu <kopeikaarkashu@gmail.com>
2022-06-30 23:44:34 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+//Task1
+let admin;
+let name;
+name = 'John';
+admin = name;
+alert(admin)
+
+//Task2
+let planetName;
+let currentUserName;
+
+//Task3
+let concat = '10' + 10;
+console.log(typeof concat) //Происходит конкатенация (склеивание)
